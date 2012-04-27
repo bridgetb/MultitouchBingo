40129837fefd44a2cf3c5c44df4f32170c8091f7
Jason Greenwood <2datop@gmail.com>
2012-04-27 17:25:40 +1200

    Jason Greenwood: Player class added, working on integration to test for victory condition
    	new file:   MultitouchBingo/ECE_700_BoardGame/ECE_700_BoardGame/Engine/Player.cs
    	modified:   MultitouchBingo/ECE_700_BoardGame/ECE_700_BoardGame/App1.cs

---
+++ MultitouchBingo/ECE_700_BoardGame/ECE_700_BoardGame/Engine/Player.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics;
+
+namespace ECE_700_BoardGame.Engine
+{
+    class Player
+    {
+        #region Field Members
+
+        List<BingoTile> PlayerTiles;
+        
+        bool[] AnsweredTiles;
+        bool HasWon;
+        bool previousAnsCorrect;
+
+        int Score;
+        int PlayerID;
+
+        #endregion
+
+        public Player(List<BingoTile> playerTiles, int playerID){
+            this.PlayerTiles = playerTiles;
+            AnsweredTiles = new bool[playerTiles.Count];
+            this.PlayerID = playerID;
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public void Initialize()
+        {
+            Score = 0;
+            HasWon = false;
+            previousAnsCorrect = false;
+
+            for (int i = 0; i < AnsweredTiles.Length; i++)
+            {
+                AnsweredTiles[i] = false;
+            }
+        }
+
+        public void tileAnswered(bool ansCorrect, int tileNum)
+        {
+            if(ansCorrect){
+                AnsweredTiles[tileNum] = true;
+
+                if(previousAnsCorrect){
+                    Score += 20;
+                }
+                else{
+                    Score += 10;
+                }
+                previousAnsCorrect = true;
+
+                //TODO check if won
+                if (false)
+                {
+                    HasWon = true;
+
+#if DEBUG
+                    Debug.WriteLine("Player " + PlayerID.ToString() + " wins!", "BINGO");
+#endif
+                }
+            }
+            else{
+
+            }
+        }
+
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public void Update()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Rendering of the Player scoring data that are placed in each players section
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
